ea2738fec498fbc4560652035595901e36496421
Michael Scofield <mscofield0@tutanota.com>
2022-05-10 19:33:55 +0200

    Serde rename to JS naming scheme

--- src/validation/requirements.rs
+++ src/validation/requirements.rs
@@ -1,6 +1,7 @@
 use crate::component::definition::TruthTable;
 
 #[derive(Debug, Clone, serde::Deserialize)]
+#[serde(rename_all = "camelCase")]
 pub struct CombinationalRequirements {
     pub max_runtime: Option<u32>,
     pub max_components: Option<u32>,
