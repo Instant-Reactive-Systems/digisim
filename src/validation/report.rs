d4f6f3914b551b530b5a8a3b177cf3ffc734337b
Michael Scofield <mscofield0@tutanota.com>
2022-05-11 22:11:49 +0200

    Revert "Fixed digisim API"

--- src/validation/report.rs
+++ src/validation/report.rs
@@ -1,13 +1,6 @@
 use crate::wasm;
 
 #[derive(Debug, Clone, PartialEq, Eq, serde::Serialize)]
-#[serde(rename_all = "camelCase")]
-pub enum ConnectorKind {
-    Input,
-    Output,
-}
-
-#[derive(Debug, Clone, PartialEq, Eq, serde::Serialize)]
 pub enum ValidationError {
     IncorrectOutputs {
         input: Vec<bool>,
@@ -16,10 +9,9 @@ pub enum ValidationError {
     },
     MaxComponentsExceeded {
         used: u32,
-        max_allowed: u32,
     },
     InvalidComponentInterface {
-        kind: ConnectorKind,
+        is_input: bool,
         expected: u32,
         actual: u32,
     },
@@ -47,15 +39,67 @@ impl ValidationReport {
 
 
 
+#[wasm::wasm_bindgen(js_class = ValidationError, getter_with_clone)]
+pub struct JsValidationError {
+    pub kind: wasm::JsString,
+    pub data: wasm::JsValue,
+}
+
 #[wasm::wasm_bindgen(js_class = ValidationReport, getter_with_clone)]
 pub struct JsValidationReport {
     pub errors: wasm::Array,
 }
 
+#[wasm::wasm_bindgen]
+impl JsValidationReport {
+    pub fn success(&self) -> bool {
+        self.errors.length() == 0
+    }
+
+    pub fn failed(&self) -> bool {
+        !self.success()
+    }
+}
+
+impl Into<JsValidationError> for ValidationError {
+    fn into(self) -> JsValidationError {
+        use ValidationError::*;
+
+        match self {
+            IncorrectOutputs { input, expected, actual } => JsValidationError {
+                kind: "incorrect-outputs".into(),
+                data: wasm::JsValue::from_serde(&serde_json::json!({
+                    "input": input,
+                    "expected": expected,
+                    "actual": actual,
+                })).unwrap(),
+            },
+            MaxComponentsExceeded { used } => JsValidationError {
+                kind: "max-components-exceeded".into(),
+                data: wasm::JsValue::from_serde(&serde_json::json!({
+                    "used": used,
+                })).unwrap(),
+            },
+            InvalidComponentInterface { is_input, expected, actual } => JsValidationError {
+                kind: "invalid-component-inputs".into(),
+                data: wasm::JsValue::from_serde(&serde_json::json!({
+                    "is_input": is_input,
+                    "expected": expected,
+                    "actual": actual,
+                })).unwrap(),
+            },
+            EmptyTruthTable => JsValidationError {
+                kind: "empty-truth-table".into(),
+                data: wasm::JsValue::UNDEFINED,
+            }
+        }
+    }
+}
+
 impl Into<JsValidationReport> for ValidationReport {
     fn into(self) -> JsValidationReport {
         JsValidationReport {
-            errors: wasm::JsValue::from_serde(&self.errors).unwrap().into(),
+            errors: wasm::JsValue::from_serde(&self).unwrap().into(),
         }
     }
 }
